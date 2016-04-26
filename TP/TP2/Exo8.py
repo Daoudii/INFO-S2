ed9434da60c93ba4c2560e07c6ff01d31fbfb010
Daoudii <ensak.exchange@gmail.com>
2016-04-26 19:30:40 +0100

    Added files via upload

--- TP/TP2/Exo8.py
+++ TP/TP2/Exo8.py
@@ -1,9 +1,10 @@
 #Somme des chiffres d'un entier
 n=int(input('entrez n : '))
+print('la somme des chiffres de n est : ')
 s=0
 while n//10!=0:
     s=s+n%10
     n=n//10
 s=s+n
-print("s=",s,sep="")    
+print("s=",s,sep="")
 input()
