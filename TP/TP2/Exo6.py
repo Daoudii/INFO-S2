e1b79bbb83627d6dde82b87e4babf56f305ca97d
Daoudii <ensak.exchange@gmail.com>
2016-04-26 10:57:33 +0100

    Added files via upload

---
+++ TP/TP2/Exo6.py
@@ -0,0 +1,9 @@
+#Total de 15 en ajoutant 3 entiers
+l=1
+for i in range(1,10):
+    for j in range(1,10):
+        for k in range(1,10):
+            if i+j+k==15 and i<=j<=k: #la 2ieme condition pour eviter la redandance
+                print('la possibilité',l,'est ',i,j,k)
+                l=l+1
+input()
